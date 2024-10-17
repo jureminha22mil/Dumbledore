d8d7c9b5ff38315315904c062ec9380c4b481b50
jureminha22mil <101815335+jureminha22mil@users.noreply.github.com>
2024-10-17 09:07:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,3 +18,9 @@ const tempoObjetivo1 = new Date("2024-10-15T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-25T00:00:00");
 const tempoObjetivo3 = new Date("2024-11-09T00:00:00");
 const tempoObjetivo4 = new Date("2024-04-12T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoAtual = tempoObjetivo - tempoAtual;
