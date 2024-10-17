04976fcca3954ab370d585154ca52c81ab2a423b
jureminha22mil <101815335+jureminha22mil@users.noreply.github.com>
2024-10-17 08:57:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,9 @@ for(let j=0; j < botoes.length; j++){
         botoes[i].classList.add("ativo");
     }
 }
+
+const contadore = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-15T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-25T00:00:00");
+const tempoObjetivo3 = new Date("2024-11-09T00:00:00");
+const tempoObjetivo4 = new Date("2024-04-12T00:00:00");
